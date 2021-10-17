6a5f5227126b7f910d65ea59e4705b69c8965664
Meikwan Chan <chan44567@gmail.com>
2021-10-17 16:04:57 -0700

    deploy react todolist to github pages

---
+++ src/components/Form.js
@@ -0,0 +1,36 @@
+import React, { useState } from "react";
+
+const Form = (props) => {
+  const [name, setName] = useState("Use hooks!");
+  const submitHandler = (event) => {
+    event.preventDefault();
+    props.addTask(name);
+    setName("");
+  };
+  const changeHandler = (event) => {
+    setName(event.target.value);
+  };
+  return (
+    <form onSubmit={submitHandler}>
+      <h2 className="label-wrapper">
+        <label htmlFor="new-todo-input" className="label__lg">
+          What needs to be done?
+        </label>
+      </h2>
+      <input
+        type="text"
+        id="new-todo-input"
+        className="input input__lg"
+        name="text"
+        autoComplete="off"
+        value={name}
+        onChange={changeHandler}
+      />
+      <button type="submit" className="btn btn__primary btn__lg">
+        Add
+      </button>
+    </form>
+  );
+};
+
+export default Form;
